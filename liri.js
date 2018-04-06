a2de0523c43d308eff62c3056b2f88f9e9e5af55
Ejeme <Ejeme@github.com>
2018-04-05 23:12:27 -0500

    Finally got my tweets back

--- liri.js
+++ liri.js
@@ -1,11 +1,13 @@
 require("dotenv").config();
 
 //Packages and Modules
-var keys = require('./keys');
-var twitter = require('twitter');
-//var spotify = require('spotify');
-var request =require('request');
 var fs =require('fs');
+var keys = require('./keys.js');
+//var client = new twitter(keys);
+var Twitter = require('twitter');
+var spotify = require('spotify');
+var request =require('request');
+
 //User input
 var userInput = process.argv[2];
 var userChoice = process.argv[3];
@@ -23,17 +25,24 @@ var tomatoURL;
 
 var parsedData;
 
-/*Functions
-==============================================================*/
 
-function twitter(inputs) {
+/*var client = new Twitter({
+    consumer_key: 'LqaijUaQLtCBfU7jsSX3XoRVN',
+    consumer_secret: 'lcBjRbzj8PYaVgluycnoYD3kMzl7t5stjrLozsab9p2cFEJ8d6',
+    access_token_key: '980959302197436422-UeJjG31CPHNWWYyB0YeV6O0aMFl5i7X',
+    access_token_secret: 'ukhDy24IZ9Ne40OUFrH4GCTiMsBxLvMbxjtpkv7ZjlYI6'
+    
+  });*/
+   
+function twitterData() {
     //User authentication credentials
-    var client = new twitter;
+    var client = new Twitter(keys.twitter);
+
 
     //Search parameters. Searches by userName
     //A maximum of 21 tweets can be returned
     var params = {
-        screen_name: 'ejeme',
+        screen_name: 'Foofa1313ejeme',
         count: 20
     };
 
@@ -49,7 +58,7 @@ function twitter(inputs) {
 
         //If there is no error...
         if (!error) {
-            //console.log("something went wrong");
+            console.log(tweets);
             for (var i = 0; i<tweets.length; i++) {
                 console.log("Created at: "+ tweets[i].created_at);
                 console.log("Text: " + tweets[i].text);
@@ -121,3 +130,112 @@ function displaySpotify(data){
   console.log('Song Preview: ' + songPreview);
 }
 */
+
+function movieThis(){
+    if(process.argv.length >=4 || typeof userChoice === 'string')    
+        
+        request(' http://www.omdbapi.com/?i=tt3896198&apikey=6afda331'
+        + userChoice +'&tomatoes=true',function(error, response, body){
+           
+            if (!error && response.statusCode === 200) {
+                  
+                  parsedData = JSON.parse(body);
+                  displayMovie(parsedData);
+              
+            }
+            else{
+              throw error;
+            }
+        });
+    else if(process.argv.length < 4){
+        
+        request('http://www.omdbapi.com/?t=' + 'Mr. Nobody' +'&tomatoes=true',function(error, response, body){
+            
+            if (!error && response.statusCode == 200) {
+                  parsedData = JSON.parse(body);
+                  displayMovie(parsedData);
+              
+            }
+            else {
+              throw error;
+            }
+        });
+    }
+}
+
+function displayMovie(parsedData){
+
+      movieTitle = parsedData['Title'];
+      movieYear = parsedData['Year'];
+      movieRating = parsedData['imdbRating'];
+      movieCountry = parsedData['Country'];
+      movieLang = parsedData['Language'];
+      moviePlot = parsedData['Plot'];
+      movieActor = parsedData['Actors'];
+      tomatoRating = parsedData['tomatoRating'];
+      tomatoURL = parsedData['tomatoURL'];
+      //Movie info
+      console.log('Movie Title: ' + movieTitle);
+      console.log('Year: ' + movieYear);
+      console.log('Imdb Rating: ' + movieRating);
+      console.log('Country: ' + movieCountry);
+      console.log('Language: ' + movieLang);
+      console.log('Plot: ' + moviePlot);
+      console.log('Actor: ' + movieActor);
+      console.log('Rotton Tomatoes Rating: ' + tomatoRating);
+      console.log('Rotton URL: ' + tomatoURL);
+
+}
+function readData(){
+
+
+
+  fs.readFile('random.txt','utf8', function(err,data){
+      if(err) throw err;
+
+      var dataSplit = data.split(',');
+          userInput = dataSplit[0];
+          userChoice = dataSplit[1];
+
+      switch(userInput){
+
+          case "my-tweets":
+            twitterData();
+            break;
+
+          case "spotify-this-song":
+            spotifyData();
+            break;
+        
+          case "movie-this":
+            movieData();
+            break;
+      }
+
+  });
+
+}
+/*Main
+==============================================================*/
+
+switch(userInput){
+     
+      case "do-what-it-says":
+          readData();
+          break;
+      
+      case "my-tweets":
+          twitterData();
+          break;
+     
+      case "spotify-this-song":
+          spotifyData();
+          break;
+      
+      case "movie-this":
+          movieData();
+          break;
+
+      default:
+          console.log('Error!!');
+}
